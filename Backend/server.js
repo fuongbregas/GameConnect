57f7071af48a6f25c7dde7b8666cd96971f133d0
fuongbregas <fuongbregas@icloud.com>
2021-03-26 22:46:08 -0700

    Get the JSON response from IGDB
    Axios call + filter worked, need to store data to MongoDB

---
+++ Backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const axios = require ('axios');
+/*
+const getGameData = () => {
+    axios({
+        method: 'post',
+        url: 'https://api.igdb.com/v4/games/',
+        headers:{
+            'Client-ID': 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn',
+            'Authorization': 'Bearer ke6pc6yxno2y2qp1g5ggbfp8bq4gg3',
+        },
+        data:{
+            fields: 'name',
+            where: 'release_dates.platform = 6',
+        }
+    }).then(response => {
+        console.log(response);
+    });
+}
+*/
+
+
+
+app.listen(3000, function(){
+    console.log('Listening on 3000');
+});
+
+app.use(() => {
+    axios({
+        method: 'post',
+        url: 'https://api.igdb.com/v4/games/',
+        headers:{
+            'Client-ID': 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn',
+            'Authorization': 'Bearer ke6pc6yxno2y2qp1g5ggbfp8bq4gg3',
+        },
+        data:'fields *; where release_dates.platform = 6 & rating >= 90 & release_dates.date >= 946735200000; limit 80;',
+    }).then(response => {
+        console.log(response);
+    });
+} );
