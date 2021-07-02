7cbdbfa02739b37f2dea9345284b99e63fdb1d59
fuongbregas <fuongbregas@icloud.com>
2021-07-02 16:03:04 -0700

    updated getGame.py
    Grab all PC games from IGDB

--- Backend/Python/getGame.py
+++ Backend/Python/getGame.py
@@ -3,17 +3,15 @@ import pymongo
 import time
 
 url ='https://api.igdb.com/v4/games/'
-# Authorization number has expiration, need to change it over time
-headers = {'Client-ID':'bvtuqo4e9i0uoscphs9pxqdrb2q2zn', 'Authorization':'Bearer pujolmx5q5189wcp56o9buaulw2y0k',}
-raw_body ='fields *; where release_dates.platform = 6 & rating >= 90 & release_dates.date >= 1616996761228; limit 10;'
-# raw_body ='fields *; where release_dates.platform = 6;'
+client_id = 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn'
+authorization = 'Bearer pujolmx5q5189wcp56o9buaulw2y0k' # Need to update/change it automatically once expired
 
 try:
     from pymongo import MongoClient
 except ImportError:
     raise ImportError('PyMongo is not installed')
 
-# MongoDB object used to insert 
+# MongoDB object used for inserting
 class MongoDB(object):
     def __init__(self, host='localhost', port=27017, database_name=None, collection_name=None):
         try:
@@ -28,25 +26,32 @@ class MongoDB(object):
             self._collection = self._database[collection_name]
 
     def insert(self, post):
-        # insert to document without duplication
+        # insert/update to DB without duplication
         self._collection.update(post, post, upsert = True)
 
 def getGames():
-    print("Test")
-    response = requests.post(url, raw_body, headers={ 'Client-ID': 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn', 'Authorization': 'Bearer pujolmx5q5189wcp56o9buaulw2y0k'})
-    # Check for connection
-    if response.status_code != 200:
-        print('Failed to get data:', response.status_code)
-    else:
-        print('Worked')
     
-    jsonResponse = (response.json())
     mongo_db = MongoDB(database_name = 'gameConnect', collection_name = 'gameData')
-    for collection in jsonResponse:
-        # Insert or Update
-        mongo_db.insert(collection)
+    offset_value = 0
+    # There are 50765 PC games on IGDB at the moment
+    for x in range(103):  
+        # Get PC games, offset 500+ to get 500 games every call
+        raw_body ='fields *; where release_dates.platform = 6; limit 500; ' + 'offset ' + str(offset_value) + ';'
+        response = requests.post(url, raw_body, headers = {'Client-ID':client_id, 'Authorization':authorization,})
         
+        # Check for connection
+        if response.status_code != 200:
+            print('Failed to get data: ', response.status_code)
+        else:
+            print('Current offset value: ' + str(offset_value))
 
+        time.sleep(2) # Sleep 2 seconds every call
+        jsonResponse = (response.json())
+        for collection in jsonResponse:
+        # Insert or Update
+            mongo_db.insert(collection)
+        offset_value = offset_value + 500 # Increases by 500 due to IGDB limitation       
+        
 getGames()
 
     
