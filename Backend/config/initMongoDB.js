d9405647783f5123b67b1e179927b9bd301a5c26
fuongbregas <fuongbregas@icloud.com>
2021-07-13 11:22:44 -0700

    Connect server code to MongoDB database

--- Backend/config/initMongoDB.js
+++ Backend/config/initMongoDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/GameConnect"
+const mongoURI = "mongodb://localhost:27017/gameConnect"
 
 const options = {
     userNewUrlParser: true,
--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const app = express();
 const connectMongoDB = require("./config/initMongoDB");
+const PORT = process.env.PORT || 3001;
 
-const axios = require ('axios');
-const { response } = require('express');
+// Connect MongoDB
+connectMongoDB();
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('Listening on 3000');
 });
-var obj = null;
-app.use(() => {
-    axios({
-        method: 'post',
-        url: 'https://api.igdb.com/v4/games/',
-        headers:{
-            'Client-ID': 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn',
-            'Authorization': 'Bearer ke6pc6yxno2y2qp1g5ggbfp8bq4gg3',
-        },
-        data:'fields *; where release_dates.platform = 6 & rating >= 90 & release_dates.date >= 946735200000; limit 80;',
-    }).then(response => {
-        obj = response.data;
-        //console.log(obj);
-        console.log(obj); // id, name, category, first_release_date, cover, rating, genres, similar_games, updated_at
-    });
-    
-} );
+
+
