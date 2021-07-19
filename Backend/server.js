3e2d48341f65aab0c0e6ba884c7f143568356337
fuongbregas <fuongbregas@icloud.com>
2021-07-18 22:43:53 -0700

    pulled data from backend to frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const connectMongoDB = require("./config/initMongoDB");
 const PORT = process.env.PORT || 3001;
+const CORS = require('cors');
 
 // Connect MongoDB
 connectMongoDB();
+
+app.use(CORS());
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
--- Frontend/game-connect/src/components/pages/Community.js
+++ Frontend/game-connect/src/components/pages/Community.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Community extends Component {
   constructor (props) {
     super(props);
     this.state = {
-
-    }
+      game : {},
+    };
   }
-  /*
+  
   componentDidMount() {
-    axios.get('/community').then((res) =>{
+    axios.get('http://localhost:3001/backend/game_data').then((res) =>{
 
+      let data = res.gameData;
+      console.log("Test \n" + JSON.stringify(res));
+      this.setState({game : data.name}); 
+      console.log(this.state.game);
     })
     .catch((error) => {
       console.log(error);
     });
   }
-  */
+  
 
   render() {
     return(
