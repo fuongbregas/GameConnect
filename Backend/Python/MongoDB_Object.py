1b38de49cd9696a692b1049efae28dd437b2a999
fuongbregas <fuongbregas@icloud.com>
2021-07-24 19:33:53 -0700

    Cleanup Python code

---
+++ Backend/Python/MongoDB_Object.py
@@ -0,0 +1,40 @@
+try:
+    from pymongo import MongoClient
+except ImportError:
+    raise ImportError('PyMongo is not installed')
+
+# MongoDB object used for inserting
+class MongoDB(object):
+    def __init__(self, host='localhost', port=27017, database_name=None, collection_name=None):
+        try:
+            self._connection = MongoClient(host=host, port=port, maxPoolSize=200)
+        except Exception as error:
+            raise Exception(error)
+        self._database = None
+        self._collection = None
+        if database_name:
+            self._database = self._connection[database_name]
+        if collection_name:
+            self._collection = self._database[collection_name]
+
+    # Insert game data
+    def insert(self, post):
+        # insert/update to DB without duplication
+        self._collection.update(post, post, upsert = True) # Warning: update() is deprecated, lazy to rewrite.
+
+    # Storing new offset value
+    def update_offset(self, post, new_value):
+        # update old offset with new offset
+        self._collection.update_one(post, new_value)
+
+    # Default value is 0   
+    def get_offset_value(self):
+        returnValues = self._collection.find_one()
+        # get offset value from Python dict
+        return returnValues['currentOffsetValue']
+
+    # Get authorization token from mongo_db
+    def get_authorization_token(self):
+        returnToken = self._collection.find_one()
+        # get token value from Python dict
+        return 'Bearer ' + str(returnToken['igdb_token']) # The full authorization is 'Bearer token_key'
--- Backend/Python/getGame.py
+++ Backend/Python/getGame.py
@@ -1,55 +1,13 @@
 import requests
 from datetime import datetime
 import time
-
+from MongoDB_Object import MongoDB
 
 # Important fields of game data from IGDB
 filter = 'id,aggregated_rating,category,cover,first_release_date,game_modes,genres,keywords,multiplayer_modes,name,rating,similar_games,summary'
 
 url ='https://api.igdb.com/v4/games/'
 client_id = 'bvtuqo4e9i0uoscphs9pxqdrb2q2zn'
-# authorization = 'Bearer pujolmx5q5189wcp56o9buaulw2y0k' # Need to update/change it automatically once expired
-
-try:
-    from pymongo import MongoClient
-except ImportError:
-    raise ImportError('PyMongo is not installed')
-
-# MongoDB object used for inserting
-class MongoDB(object):
-    def __init__(self, host='localhost', port=27017, database_name=None, collection_name=None):
-        try:
-            self._connection = MongoClient(host=host, port=port, maxPoolSize=200)
-        except Exception as error:
-            raise Exception(error)
-        self._database = None
-        self._collection = None
-        if database_name:
-            self._database = self._connection[database_name]
-        if collection_name:
-            self._collection = self._database[collection_name]
-
-    # Insert game data
-    def insert(self, post):
-        # insert/update to DB without duplication
-        self._collection.update(post, post, upsert = True) # Warning: update() is deprecated, lazy to rewrite.
-
-    # Storing new offset value
-    def update_offset(self, post, new_value):
-        # update old offset with new offset
-        self._collection.update_one(post, new_value)
-
-    # Default value is 0   
-    def get_offset_value(self):
-        returnValues = self._collection.find_one()
-        # get offset value from Python dict
-        return returnValues['currentOffsetValue']
-
-    # Get authorization token from mongo_db
-    def get_authorization_token(self):
-        returnToken = self._collection.find_one()
-        # get token value from Python dict
-        return 'Bearer ' + str(returnToken['igdb_token']) # The full authorization is 'Bearer token_key'
 
 # Convert seconds to_epoch_time_converter, ISO 8061
 def time_converter(epoch_time):
