c4b0b1d3813ce8b9ab5b4dbccccca58e85addd22
fuongbregas <fuongbregas@icloud.com>
2021-07-25 20:06:48 -0700

    added login route

--- Backend/routes/authorization.js
+++ Backend/routes/authorization.js
@@ -6,18 +6,35 @@ const bcrypt = require('bcrypt');
 const User = require('../models/Users/UserSchema');
 
 // Register User
+router.post('/register', async (req, res) => {
+    try {
+        // generate hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+        // create new user
+    }
+
+    catch {
+        res.status(500).json(err);
+    }
+});
 
 // Login User
 router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne ({email: req.body.email});
+        const user = await User.findOne ({email: req.body.email}); // find the user in MongoDB with the given email
         !user && res.status(404).json("User not found");
         
-        cons
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Wrong password");
+
+        res.status(200).json(user);
     }
 
     catch (err) {
-
+        res.status(500).json(err);
     }
 });
+
+module.exports = router;
