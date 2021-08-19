908afd5f35b0359db067dce884b58e920cb50a0f
Vignesh <vignesht.96@gmail.com>
2021-08-18 18:50:14 -0700

    Testing new branch push, added small code to try and grab data from backend

--- Frontend/game-connect/src/pages/Home/index.js
+++ Frontend/game-connect/src/pages/Home/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+function displayGameData() {
+  axios.get('/backend/game_data').then((res) =>{
+
+    let data = res.gameData;
+    console.log("Test \n" + JSON.stringify(res));
+    this.setState({game : data.name}); 
+    console.log(this.state.game);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+
 export default function Home() {
     return(
         <>
