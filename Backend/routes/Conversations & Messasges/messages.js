ab484bd632d00cfbc0bd1265f66a2837f8fe02b9
fuongbregas <fuongbregas@icloud.com>
2021-09-17 01:16:57 -0700

    added deleted message to front end

--- Backend/routes/Conversations & Messasges/messages.js
+++ Backend/routes/Conversations & Messasges/messages.js
@@ -15,14 +15,25 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Replace message_content of deleted message
+
+
 // Get all messages from a conversation ID
 router.get('/:conversation_id', async (req, res) => {
     try {
         const message = await Message.find({
             conversation_id : req.params.conversation_id,
-        });
+        }).lean();
+        const deleted_message = 'This message has been delete.';
+        for(var i = 0; i < message.length; i++) {
+            if(message[i].is_deleted === true){
+                message[i].message_content = deleted_message;
+            }
+        }
+        
         
         res.status(200).send(message);
+        
     }
     catch (error) {
         res.status(500).json(error);
