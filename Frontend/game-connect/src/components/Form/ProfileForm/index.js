2af6fb8cb971ddc0d9aa38418ddb05abcea0cd53
fuongbregas <fuongbregas@icloud.com>
2021-10-20 00:42:41 -0700

    Working on Profile

---
+++ Frontend/game-connect/src/components/Form/ProfileForm/index.js
@@ -0,0 +1,13 @@
+import './ProfileElements.css';
+import ProfileTabs from '../../ProfileComponents/ProfileTabs/ProfileTabs';
+
+const Profile = () => {
+    return (
+        <div className="profileContainer">
+            <ProfileTabs/>
+        </div>
+    );
+}
+
+export default Profile;
+
---
+++ Frontend/game-connect/src/components/ProfileComponents/ProfilePicture/ProfilePicture.jsx
@@ -0,0 +1,14 @@
+import {React, useState, useContext} from 'react';
+import './ProfilePicture.css';
+
+const ProfilePicture = () => {
+    const [profilePictureURL, setProfilePictureURL] = useState('');
+    
+    return (
+        <div>
+            <img src = '' alt = ''/>
+        </div>
+    );
+}
+
+export default ProfilePicture;
