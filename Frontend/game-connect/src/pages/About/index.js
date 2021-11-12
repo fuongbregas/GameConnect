5f4b1d07bb79c420006fdb14dce614368e0dbd2f
fuongbregas <fuongbregas@icloud.com>
2021-11-12 11:41:10 -0800

    CSS stuff

--- Frontend/game-connect/src/pages/About/index.js
+++ Frontend/game-connect/src/pages/About/index.js
@@ -11,7 +11,7 @@ export default function About() {
             Resize the browser window to see that this page is responsive by the way.
           </p>
         </div>
-        <div className = 'container'>
+        <div className = 'team-container'>
           <h1 className='team'>Our team</h1>
         </div>
         
