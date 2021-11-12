4ef8d315cb4327fca0faa5e2639cdc42f3e5f468
fuongbregas <fuongbregas@icloud.com>
2021-11-11 20:51:49 -0800

    working on About Us

--- Frontend/game-connect/src/pages/About/index.js
+++ Frontend/game-connect/src/pages/About/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './About.css';
 
 export default function About() {
-  
     return(
-      <div>About</div>
+      <>
+        <div className="about-section">
+          <h1>About Us</h1>
+          <p>We are guys who enjoy gaming on PC.</p>
+          <p>
+            Resize the browser window to see that this page is responsive by the way.
+          </p>
+        </div>
+        <div className = 'container'>
+          <h1 className='team'>Our team</h1>
+        </div>
+        
+      </>
     );
 }
