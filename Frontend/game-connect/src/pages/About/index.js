6c4d7d7f97a1fee1b9202332109e8e6664574dad
fuongbregas <fuongbregas@icloud.com>
2021-11-16 21:45:34 -0800

    added routes to server

--- Frontend/game-connect/src/pages/About/index.js
+++ Frontend/game-connect/src/pages/About/index.js
@@ -14,7 +14,29 @@ export default function About() {
         <div className = 'team-container'>
           <h1 className='team'>Our team</h1>
           <div className='people-container'>
-            
+            <div className = 'individual'>
+              <div className='individual-container'>
+                Phuong
+              </div>
+            </div>
+
+            <div className = 'individual'>
+              <div className='individual-container'>
+                Vig
+              </div>
+            </div>
+
+            <div className = 'individual'>
+              <div className='individual-container'>
+                Vu
+              </div>
+            </div>
+
+            <div className = 'individual'>
+              <div className='individual-container'>
+                Ed
+              </div>
+            </div>
           </div>
         </div>
         
